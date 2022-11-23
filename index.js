f9467a1be1a376a0ad234f05d285bfbc1047493d
Musyoki_Wambua <wjosephmusyoki@gmail.com>
2022-11-23 14:43:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+
+const currentUser = "Musyoki Wambua";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+
+
+
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0 ,1)}!`;
